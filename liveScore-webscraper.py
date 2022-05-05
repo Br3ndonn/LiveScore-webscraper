fcb423d13b3dff595a7272df88af355e74b74de7
Brendon Silva <96079864+Br3ndonn@users.noreply.github.com>
2022-05-04 23:29:40 -0300

    Update liveScore-webscraper.py

--- liveScore-webscraper.py
+++ liveScore-webscraper.py
@@ -1,12 +1,47 @@
 from datetime import date, timedelta
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 from twilio.rest import Client
 import os
 
 dados_partidas = dict()
 lista_de_partidas = list()
 
+
+def menu():
+    resposta = int(input('1. Apenas Ver tabela'
+                         '\n2. Enviar Whatsapp'
+                         '\n3. Pesquisa time específico'
+                         '\n4. Pesquisa horário'
+                         '\n5. Deleta arquivo txt'))
+
+    if resposta == 1:
+        print(cria_arquivo_txt())
+        menu()
+    if resposta == 2:
+        envia_whatsapp()
+        menu()
+    if resposta == 3:
+        pesquisa_times_especificos()
+        menu()
+    if resposta == 4:
+        pesquisa_horario()
+        menu()
+    if resposta == 5:
+        deleta_arquivo_txt()
+
+        
+def cria_arquivo_txt():
+    with open('tabela' + str(f'{date.today() + timedelta(days=1)}') + '.txt', 'w', encoding="utf-8") as arquivo:
+        for valor in lista_de_partidas:
+            horario, time_casa, time_fora = valor.values()         
+            arquivo.write(f'{horario}: {time_casa} x {time_fora}' + '\n')
+
+    with open('tabela' + str(f'{date.today() + timedelta(days=1)}') + '.txt', 'r', encoding="utf-8") as arquivo2:
+        arquivo_txt = arquivo2.read()
+                
+    return arquivo_txt
+        
 
 def pega_url_com_data():
     """Pega a data desejada e concatena com a URL"""
@@ -21,41 +56,18 @@ def pega_url_com_data():
 def envia_whatsapp():
     """Envia a tabela de jogos pelo whatsapp se o usário desejar"""
     
-    resposta = input('Deseja enviar horário dos jogos para o whatsapp?[S/N]')
-    if resposta in 'Ss':
-        account_sid = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-        auth_token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-        client = Client(account_sid, auth_token)
+    account_sid = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' # Your twillio tokens
+    auth_token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+    client = Client(account_sid, auth_token)
 
-        message = client.messages.create(
-            from_='whatsapp:+xxxxxxxxxxx',
-            body=f'{str(cria_arquivo_txt()[:1599])}',
-            to='whatsapp:+xxxxxxxxxx'
-        )
+    message = client.messages.create(
+        from_='whatsapp:+xxxxxxxxxxx',
+        body=f'{str(cria_arquivo_txt()[:1599])}',
+        to='whatsapp:+xxxxxxxxxx'
+    )
 
-        print(message.sid)
-        print(cria_arquivo_txt())
-        deleta_arquivo_txt()
-    else:
-        print(cria_arquivo_txt())
-        deleta_arquivo_txt()
-
-
-def cria_arquivo_txt():
-    with open('tabela' + str(f'{date.today() + timedelta(days=1)}') + '.txt', 'w', encoding="utf-8") as arquivo:
-        for valor in lista_de_partidas:
-            if bool(valor):
-                arquivo.write(str(valor.values()) + '\n')
-
-    with open('tabela' + str(f'{date.today() + timedelta(days=1)}') + '.txt', 'r', encoding="utf-8") as arquivo2:
-        arquivo_txt = arquivo2.read()
-        caracteres_a_remover = ['dict_values([', '])']
-
-        for x in range(len(arquivo_txt)):
-            for y in range(len(caracteres_a_remover)):
-                arquivo_txt = arquivo_txt.replace(caracteres_a_remover[y], "")
-                
-    return arquivo_txt
+    print(message.sid)
+    print(cria_arquivo_txt())
 
 
 def deleta_arquivo_txt():
@@ -64,30 +76,60 @@ def deleta_arquivo_txt():
     nome_arquivo = 'tabela' + str(f'{date.today() + timedelta(days=1)}') + '.txt'
     if os.path.exists(nome_arquivo):
         os.remove(nome_arquivo)
-        
 
+        
+def pesquisa_times_especificos():
+    times = []
+    cria_arquivo_txt()
+
+    while True:
+        times.append(input(f'Adicione time: ').capitalize())
+        # Teclar Enter quando não quiser adicionar mais times. Tornando o último elemento vazio.
+        if times[len(times) - 1] == '':
+            # Remove a último elemento.
+            times.pop()
+            break
+    with open('tabela' + str(f'{date.today() + timedelta(days=1)}') + '.txt', 'r', encoding="utf-8") as arquivo:
+        for linha in arquivo:
+            for time in times:
+                if time in linha:
+                    print(linha)        
+        
+        
+def pesquisa_horario():
+    cria_arquivo_txt()
+    hora = input('Horario do jogo: ')
+    with open('tabela' + str(f'{date.today() + timedelta(days=1)}') + '.txt', 'r', encoding="utf-8") as arquivo:
+        for linha in arquivo:
+            if f'{hora}:' in linha:
+                print(linha)
+    
+    
 # REQUEST DO SITE
 r = requests.get(pega_url_com_data())
 soup = BeautifulSoup(r.content, 'html.parser')
 
 
 # PERCORRE O CÓDIGO HTML EM BUSCA DOS ELEMENTOS DESEJADOS
-for div in soup.find(class_='MatchRows_root__1NKae'):
+for div in soup.find(class_='J'):
     for partidas in div.find_all('div'):
 # PERCORRE AS DIV COLETANDO OS HORÁRIOS, MANDANTE DO JOGO E TIME VISITANTE
 # SE A DIV NÃO ESTIVER VAZIA, O ELEMENTO É SALVO EM UM DICIONÁRIO E SALVA O DICIONÁRIO EM UMA LISTA PARA QUE POSSAMOS PEGAR OS DEMAIS VALORES
         dados_partidas.clear()
-        if partidas.find('span', class_='CategoryHeader_categoryHeaderWrapper__33fmX') is not None:
-            dados_partidas['campeonato'] = partidas.find(class_='CategoryHeader_stage__1lhRX').get_text()
-        if partidas.find('span', class_='MatchRowTime_time__2Fkd2 MatchRowTime_justifyContentStart__3XvwU') is not None:
-            dados_partidas['horario'] = partidas.find('span', class_='MatchRowTime_time__2Fkd2 MatchRowTime_justifyContentStart__3XvwU').get_text()
-        if partidas.find(class_='FootballMatchRow_teamName__28Hxv') is not None:
-            dados_partidas['time_casa'] = partidas.find(class_='FootballMatchRow_teamName__28Hxv').get_text()
-        if partidas.find(class_='FootballMatchRow_away__12Br8') is not None:
-            dados_partidas['time_fora'] = partidas.find(class_='FootballMatchRow_away__12Br8').get_text()
-        lista_de_partidas.append(dados_partidas.copy())
-
-# SALVA A LISTA DE DICIONÁRIOS COMO STR EM UM ARQUIVO .txt SE O DICIONÁRIO NÃO ESTIVER VAZIO
-
-
-envia_whatsapp()
+    
+        if partidas.find(class_='eb') is not None:
+            dados_partidas['campeonato'] = partidas.find('span', class_='hb').get_text()
+        
+        if partidas.find('span', class_='Gh') is not None:
+            dados_partidas['horario'] = partidas.find('span', class_='ug qg').get_text()
+        
+        if partidas.find(class_='Gh') is not None:
+            dados_partidas['time_casa'] = partidas.find(class_='Nh').get_text()
+        
+        if partidas.find(class_='Gh') is not None:
+            dados_partidas['time_fora'] = partidas.find(class_='Mh').get_text()
+        
+        if bool(dados_partidas):
+            lista_de_partidas.append(dados_partidas.copy())
+            
+menu()
