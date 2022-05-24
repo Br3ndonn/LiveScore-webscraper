8111b56a566c1b066e42e59034ade37ec55db3e3
Brendon Silva <96079864+Br3ndonn@users.noreply.github.com>
2022-05-24 15:15:39 -0300

    Translated to english

--- liveScore-webscraper.py
+++ liveScore-webscraper.py
@@ -4,48 +4,51 @@ from bs4 import BeautifulSoup
 from twilio.rest import Client
 import os
 
-dados_partidas = {}
-lista_de_partidas = []
+matchs_data = {}
+matchs_list = []
 
 
-# FUNÇÕES
 def menu():
-    resposta = int(input('1. Apenas Ver tabela'
-                         '\n2. Enviar Whatsapp'
-                         '\n3. Pesquisa time específico'
-                         '\n4. Pesquisa horário'
-                         '\n5. Deleta arquivo txt'))
+    """Menu with user's options."""
 
-    if resposta == 1:
-        print(cria_arquivo_txt())
+    answer = int(input('1. See matchs only'
+                         '\n2. Send Whatsapp message'
+                         '\n3. Look for specifics TEAM'
+                         '\n4. Look for Specific HOUR'
+                         '\n5. Delete .txt file'))
+
+    if answer == 1:
+        print(create_file_txt())
         menu()
-    if resposta == 2:
-        envia_whatsapp()
+    if answer == 2:
+        send_whatsapp()
         menu()
-    if resposta == 3:
-        pesquisa_times_especificos()
+    if answer == 3:
+        specific_team_search()
         menu()
-    if resposta == 4:
-        pesquisa_horario()
+    if answer == 4:
+        search_by_match_hour()
         menu()
-    if resposta == 5:
-        deleta_arquivo_txt()
+    if answer == 5:
+        delete_txt_file()
 
 
-def cria_arquivo_txt():
-    with open('tabela' + str(f'{date.today() + timedelta(days=1)}') + '.txt', 'w', encoding="utf-8") as arquivo:
-        for valor in lista_de_partidas:
-            horario, time_casa, time_fora = valor.values()
-            arquivo.write(f'{horario}: {time_casa} x {time_fora}' + '\n')
+def create_file_txt():
+    """Create .txt file"""
 
-    with open('tabela' + str(f'{date.today() + timedelta(days=1)}') + '.txt', 'r', encoding="utf-8") as arquivo2:
-        arquivo_txt = arquivo2.read()
+    with open('matchs' + str(f'{date.today() + timedelta(days=1)}') + '.txt', 'w', encoding="utf-8") as file:
+        for value in scraper():
+            schedule, home_team, away_team = value.values()
+            file.write(f'{schedule}: {home_team} x {away_team}' + '\n')
 
-    return arquivo_txt
+    with open('matchs' + str(f'{date.today() + timedelta(days=1)}') + '.txt', 'r', encoding="utf-8") as file2:
+        file_txt = file2.read()
+
+    return file_txt
 
 
-def pega_url_com_data():
-    """PEGA A DATA DESEJADA(HOJE OU AMANHÃ) E CONCATENA COM A URL DO SITE"""
+def get_url_with_date():
+    """GET THE DESIGNATED DATE (TODAY OR TOMORROW) AND CONCATEN IT WITH THE SITE'S URL"""
 
     tomorrows_date = date.today() + timedelta(days=1)
     # today = "http://www.livescores.com/football/" + str(date.today()) + "/?tz=-3"
@@ -54,7 +57,8 @@ def pega_url_com_data():
     return next_day_url
 
 
-def envia_whatsapp():
+def send_whatsapp():
+    """Send the matchs list through whatsapp"""
 
     account_sid = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
     auth_token = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
@@ -62,67 +66,76 @@ def envia_whatsapp():
 
     message = client.messages.create(
         from_='whatsapp:+XXXXXXXXXXXX',
-        body=f'{str(cria_arquivo_txt()[:1599])}',
+        body=f'{str(create_file_txt()[:1599])}',
         to='whatsapp:+XXXXXXXXXXXX'
     )
 
     print(message.sid)
-    print(cria_arquivo_txt())
+    print(create_file_txt())
 
 
-def deleta_arquivo_txt():
-    """Deleta arquivo .txt gerado após lista de jogos ser mostrada ao usuário"""
+def delete_txt_file():
+    """Delete .txt file generated after matchs's list is shown to user."""
 
-    nome_arquivo = 'tabela' + str(f'{date.today() + timedelta(days=1)}') + '.txt'
-    if os.path.exists(nome_arquivo):
-        os.remove(nome_arquivo)
+    file_name = 'matchs' + str(f'{date.today() + timedelta(days=1)}') + '.txt'
+    if os.path.exists(file_name):
+        os.remove(file_name)
 
 
-def pesquisa_times_especificos():
-    times = []
-    cria_arquivo_txt()
+def specific_team_search():
+    """Search for a specific team."""
+
+    teams = []
+    create_file_txt()
 
     while True:
-        times.append(input(f'Adicione time: ').capitalize())
-        # Teclar Enter quando não quiser adicionar mais times. Tornando o último elemento vazio.
-        if times[len(times) - 1] == '':
-            # Remove a último elemento.
-            times.pop()
+        teams.append(input(f'Add team: ').capitalize())
+        # Press Enter when don't want add another time. Making the last element empty
+        if teams[len(teams) - 1] == '':
+            # Remove the last element.
+            teams.pop()
             break
-    with open('tabela' + str(f'{date.today() + timedelta(days=1)}') + '.txt', 'r', encoding="utf-8") as arquivo:
-        for linha in arquivo:
-            for time in times:
-                if time in linha:
-                    print(linha)
+    with open('matchs' + str(f'{date.today() + timedelta(days=1)}') + '.txt', 'r', encoding="utf-8") as file:
+        for line in file:
+            for team in teams:
+                if team in line:
+                    print(line)
 
 
-def pesquisa_horario():
-    cria_arquivo_txt()
-    hora = input('Horario do jogo: ')
-    with open('tabela' + str(f'{date.today() + timedelta(days=1)}') + '.txt', 'r', encoding="utf-8") as arquivo:
-        for linha in arquivo:
-            if f'{hora}:' in linha:
-                print(linha)
+def search_by_match_hour():
+    """Search for a specific hour of the day."""
+
+    create_file_txt()
+    hour = input('Hour: ')
+    with open('matchs' + str(f'{date.today() + timedelta(days=1)}') + '.txt', 'r', encoding="utf-8") as file:
+        for line in file:
+            if f'{hour}:' in line:
+                print(line)
 
 
-r = requests.get(pega_url_com_data())
-soup = BeautifulSoup(r.content, 'html.parser')
+def scraper():
+    """Do the scrape"""
 
-for div in soup.find(class_='J'):
-    for partidas in div.find_all('div'):
-        dados_partidas.clear()
-        if partidas.find(class_='eb') is not None:
-            dados_partidas['campeonato'] = partidas.find('span', class_='hb').get_text()
+    r = requests.get(get_url_with_date())
+    soup = BeautifulSoup(r.content, 'html.parser')
 
-        if partidas.find(class_='Gh') is not None:
-            dados_partidas['horario'] = partidas.find('span', class_='ug qg').get_text()
+    for div in soup.find(class_='J'):
+        for matchs in div.find_all('div'):
+            matchs_data.clear()
+            if matchs.find(class_='eb') is not None:
+                matchs_data['league'] = matchs.find('span', class_='hb').get_text()
 
-        if partidas.find(class_='Gh') is not None:
-            dados_partidas['time_casa'] = partidas.find(class_='Nh').get_text()
+            if matchs.find(class_='Gh') is not None:
+                matchs_data['schedule'] = matchs.find('span', class_='ug qg').get_text()
 
-        if partidas.find(class_='Gh') is not None:
-            dados_partidas['time_fora'] = partidas.find(class_='Mh').get_text()
-        if bool(dados_partidas):
-            lista_de_partidas.append(dados_partidas.copy())
+            if matchs.find(class_='Gh') is not None:
+                matchs_data['home_team'] = matchs.find(class_='Nh').get_text()
+
+            if matchs.find(class_='Gh') is not None:
+                matchs_data['away_team'] = matchs.find(class_='Mh').get_text()
+            
+            if bool(matchs_data):
+                matchs_list.append(matchs_data.copy())
+
 
 menu()
